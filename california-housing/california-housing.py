0c1df9d9b1e7739df9163dc05cd261e612e3dbad
Rinto Cherian <chrint223@gmail.com>
2026-07-14 22:22:14 +0530

    add notebook and py files

---
+++ california-housing/california-housing.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+housing = fetch_california_housing()
+
+df = pd.DataFrame(housing.data, columns=housing.feature_names)
+df["PRICE"] = housing.target
+
+print(df.head())
+
+X = df.drop("PRICE", axis=1)
+y = df["PRICE"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test, y_pred)
+
+print("MSE :", mse)
+print("RMSE:", rmse)
+print("R² Score:", r2)
+
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual Prices")
+plt.ylabel("Predicted Prices")
+plt.title("Actual vs Predicted Prices")
+plt.show()
